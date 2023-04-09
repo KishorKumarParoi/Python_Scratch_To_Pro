7deb03a680cb678d2910c0a01a682ee6d42bdf38
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-04-09 18:08:05 +0600

    Learned Variables,data-types,tuples,dictionary,list etc

---
+++ 05-Variables-Data-Types/main.py
@@ -0,0 +1,36 @@
+a = 12324424122555255
+print(a)
+Kishor = 99
+b = "Kishor"
+print("The type of b ", type(b))
+print(b)
+b = Kishor
+print(b)
+print(a + b)
+print(a * b)
+print("The type of a ", type(a))
+print("The type of b ", type(b))
+print("The type of a+b ", type(a + b))
+d = True
+print("The type of d ", type(d))
+d = None
+print("The type of d ", type(d))
+e = complex(8, 2)
+print("The type of e ", e, type(e))
+f = 1.3234324244445235352
+print("The type of f ", f, type(f))
+
+list1 = [9, 23, 24, [-344, 24], ["apple", "komla", "tormuj"]]
+print(list1)
+
+tuple1 = (("parrot", "Kakku"), ("Takle", ("habagoba", "goru"), "Foring"),
+          "Tiger")
+print(tuple1)
+
+# dictionary is key-value pair
+dict1 = {"Name": "Kishor", "Age": 25, "canVote": True}
+print(dict1)
+
+# Everything is object in python
+# tuple immutable
+# list mutable -> change can be done
