df746e58ee9c4272121257764c2cfe35433b4eda
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-04-09 07:54:22 +0600

    Learned comments, Sep, end, escape sequence

---
+++ 04-Comments-Escape-Sequence-Print/main.py
@@ -0,0 +1,13 @@
+'''
+# Comments in Python
+# Multicomments in
+# Python
+'''
+
+print("Hey I am a good boy doing CP\nand I want to get 5 crore Package Job")
+print(
+  "I want to learn typescript, react, k6, smart contracts, solidity, postgres etc backend thing"
+)
+
+print("hey", 6, 7, 8, sep="~", end="009\n")
+print("Kishor Kumar Paroi")
