8d6efd153573bdad2c1e0c712febbf3b3fd4b4db
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-04-10 14:28:59 +0600

    learned taking user input

---
+++ 08-User-Input/main.py
@@ -0,0 +1,9 @@
+a = input("Enter Your Name : ")
+print("My Name is : ", a)
+
+x = input("Enter First Number : ")
+y = input("Enter Second Number : ")
+
+print(type(x))
+print(x + y)
+print(int(x) + int(y))
