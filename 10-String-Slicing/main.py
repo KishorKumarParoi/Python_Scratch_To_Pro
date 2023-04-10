2dcc930851733f87098331e8275e350746cdc827
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-04-10 23:06:34 +0600

    Learned String Slicing

---
+++ 10-String-Slicing/main.py
@@ -0,0 +1,13 @@
+names = "Kishor, Pallabi"
+print(names[0:6])
+print(len(names))
+print("Kishor is a", len("Kishor is a"), " letter word")
+print(names[5:15])
+print(names[:5])
+
+print(names[5:-3])
+print(names[-1:len(names) - 10])
+print(names[-15:-9])
+# len - 15 : len - 9
+nm = "Kishor"
+print(nm[-4:-2])
