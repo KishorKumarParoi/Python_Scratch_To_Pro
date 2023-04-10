54eea549051dd04561e23cf60885ce78e1c2a7c4
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-04-10 15:12:52 +0600

    Learned Strings Operation

---
+++ 09-Strings/main.py
@@ -0,0 +1,16 @@
+Name = "Kishor Kumar Paroi"
+friend = "Pallabi Karmaker"
+allOf = """"Kishor Kumar Paroi, Pallabi's Home, "KKP's basa"
+"""
+print(allOf)
+print('hello ' + Name + " -> " + friend)
+print(Name[3])
+print(allOf[5])
+
+print("\n")
+# For Loop With String
+for x in Name:
+  print(x)
+
+for x in allOf:
+  print(x)
