7a9b11d895b08492dcfa14dced4152c43b662477
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-04-10 13:58:48 +0600

    learned typecasting in python

---
+++ 07-Typecasting/main.py
@@ -0,0 +1,17 @@
+a = '1'
+b = "2"
+a = 10
+# print(type(a))
+print(str(a) + b)
+
+# if I change/convert : explicit
+# if python converts : implicit
+
+# int(), float(), hex(), oct(), str()
+# ordering in python are not same
+# Implicit Type Casting
+c = 10.23
+d = 9
+# f = "23"
+f = True
+print(c + f + d)
