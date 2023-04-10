c1d610982ed9ee2a46154a917e979bb010856703
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-04-10 10:42:25 +0600

    Created a basic calculator

---
+++ 06-Create-A-Calculator/main.py
@@ -0,0 +1,21 @@
+# print(5+6)
+# print(5-6)
+# print(5*6)
+# print(15/6)
+# print(17//6)
+# print(15%6)
+# print(5**4)
+# print(2**4)
+
+firstValue = input("Enter First Value : ")
+secondValue = input("Enter Second Value : ")
+# operator = input("Enter Operator : ")
+
+# if operator == "+" :
+print("Value is : ", int(firstValue) + int(secondValue))
+print("Value is : ", int(firstValue) - int(secondValue))
+print("Value is : ", int(firstValue) * int(secondValue))
+print("Value is : ", int(firstValue) / int(secondValue))
+print("Value is : ", int(firstValue) // int(secondValue))
+print("Value is : ", int(firstValue) % int(secondValue))
+print("Value is : ", int(firstValue) ** int(secondValue))
