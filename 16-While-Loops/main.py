4c7a410113c2ce81c73521c65a5453b777f1f5bb
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-04-11 19:51:57 +0600

    learned while loop operations,else in while loop

---
+++ 16-While-Loops/main.py
@@ -0,0 +1,24 @@
+for i in range(3):
+  print(i)
+
+print("\n")
+
+i = 0
+while (i <= 3):
+  print(i)
+  i = i + 1
+
+print("\n")
+inp = int(input("Enter the number : "))
+while (inp <= 34):
+  print(inp)
+  inp = int(input("Enter the number : "))
+
+count = 5
+while (count > 0):
+  print(count)
+  count = count - 1
+else:
+  print("I am outside")
+
+# do-while isn't in python
