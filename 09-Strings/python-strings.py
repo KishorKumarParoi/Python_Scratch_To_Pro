34152ea7a3939542a663c0cbdc3b102daee0ffdb
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-16 00:14:59 +0600

    learned escape character

--- 09-Strings/python-strings.py
+++ 09-Strings/python-strings.py
@@ -56,3 +56,28 @@ print(order.format(apple, komla, jam))
 
 order = "I order {c} apples, {b} komla and {a} jam"
 print(order.format(a=apple, b=komla, c=jam))
+
+# Escape Character
+txt = 'We are the so-called "Vikings" from the north.'
+print(txt)
+
+txt = "This will insert one \\ (backslash)."
+print(txt)
+
+txt = "Hello\nWorld!"
+print(txt)
+
+txt = "Hello\rWorld!"
+print(txt)
+
+txt = "Hello\tWorld!"
+print(txt)
+
+txt = "Hello \bWorld!"
+print(txt)
+
+txt = "\110\145\154\154\157"
+print(txt)
+
+txt = "\x48\x65\x6c\x6c\x6f"
+print(txt)
