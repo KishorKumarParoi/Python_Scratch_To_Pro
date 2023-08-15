8879c09693efddb8fea59d60ed42fe880d8afa62
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-15 15:30:22 +0600

    learned purpose and utility of python

---
+++ 01-Intro-To-Python/python.py
@@ -0,0 +1,20 @@
+# What can python do for you?
+# Python is a high-level, interpreted, interactive and object-oriented scripting language. Python is designed to be highly readable. It uses English keywords frequently where as other languages use punctuation, and it has fewer syntactical constructions than other languages.
+# web development (server side)
+# software development
+# mathematics
+# system scripting
+# alongside software to create workflows
+# connect to database files, read and modify files
+# perform complex mathematics
+# rapid prototyping for production ready software development
+# data analysis
+
+""" 
+Python is a general-purpose interpreted, interactive, object-oriented, 
+and high-level programming language. It was created by Guido van Rossum
+ during 1985- 1990. Like Perl, Python source code is also available u
+ nder the GNU General Public License (GPL). This tutorial gives enough 
+ understanding on Python programming language.
+"""
+print("leanred purpose and utility of python")
