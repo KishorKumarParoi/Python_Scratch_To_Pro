dae02204d9055bddedefada0a899ae5a9acbe77d
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-15 23:58:34 +0600

    learned in keyword, length, and checking if a word exists in string or not

--- 09-Strings/python-strings.py
+++ 09-Strings/python-strings.py
@@ -9,3 +9,17 @@ print(a[-2:])
 print(a[1:-2])
 # print(a[1:-2:2])
 # print(a[1:-2:3])
+
+for x in "banana":
+    print(x)
+
+print(len(a))
+
+txt = "The best things in life are free!"
+print("free" in txt)
+
+if "free" in txt:
+    print("Yes, 'free' is present.")
+
+if "expensive" not in txt:
+    print("No, 'expensive' is NOT present.")
