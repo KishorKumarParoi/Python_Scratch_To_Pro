a800c32c300a4c64df1e56cb99aaf3af4b87c201
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-15 15:53:29 +0600

    python allow many values assigning multiple variables to a single line

---
+++ 05-Variables-Data-Types/variable-naming.py
@@ -0,0 +1,26 @@
+"""
+A variable can have a short name (like x and y) or a more descriptive name (age, carname, total_volume). Rules for Python variables:
+A variable name must start with a letter or the underscore character
+A variable name cannot start with a number
+A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+Variable names are case-sensitive (age, Age and AGE are three different variables)
+A variable name cannot be any of the Python keywords.
+"""
+# legal variable names:
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+# illegal variable names:
+# 2myvar = "John"
+# my-var = "John"
+# my var = "John"
+
+# many values to multiple variables
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
