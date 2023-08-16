c2860a9b89f381b373412c67da47b726bacbe9d1
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-08-16 08:14:26 +0600

    learned all string-methods

--- 11-String-Methods/String-methods.py
+++ 11-String-Methods/String-methods.py
@@ -125,3 +125,116 @@ print(a.isidentifier())
 print(b.isidentifier())
 print(c.isidentifier())
 print(d.isidentifier())
+
+txt = "hello world!"
+x = txt.islower()
+print(x)
+
+txt = "565543"
+x = txt.isnumeric()
+print(x)
+
+txt = "Hello! Are you #1?"
+x = txt.isprintable()
+print(x)
+
+txt = "   "
+x = txt.isspace()
+print(x)
+
+txt = "Hello, And Welcome To My World!"
+x = txt.istitle()
+print(x)
+
+txt = "THIS IS NOW!"
+x = txt.isupper()
+print(x)
+
+myTuple = ("John", "Peter", "Vicky")
+x = "#".join(myTuple)
+print(x)
+
+txt = "banana"
+x = txt.ljust(20)
+print(x, "is my favorite fruit.")
+
+txt = "Hello my FRIENDS"
+x = txt.lower()
+print(x)
+
+txt = "     banana     "
+x = txt.lstrip()
+print("of all fruits", x, "is my favorite")
+
+txt = "Hello Sam!"
+mytable = txt.maketrans("S", "P")
+print(txt.translate(mytable))
+
+txt = "I could eat bananas all day"
+x = txt.partition("bananas")
+print(x)
+
+txt = "I like bananas"
+x = txt.replace("bananas", "apples")
+print(x)
+
+txt = "Mi casa, su casa."
+x = txt.rfind("casa")
+print(x)
+
+txt = "Mi casa, su casa."
+x = txt.rindex("casa")
+print(x)
+
+txt = "banana"
+x = txt.rjust(20)
+
+print(x, "is my favorite fruit.")
+
+txt = "I could eat bananas all day, bananas are my favorite fruit"
+x = txt.rpartition("bananas")
+print(x)
+
+txt = "apple, banana, cherry"
+x = txt.rsplit(", ")
+print(x)
+
+txt = "     banana     "
+x = txt.rstrip()
+print("of all fruits", x, "is my favorite")
+
+txt = "welcome to the jungle"
+x = txt.split()
+print(x)
+
+txt = "Thank you for the music\nWelcome to the jungle"
+x = txt.splitlines()
+print(x)
+
+txt = "Hello, welcome to my world."
+x = txt.startswith("Hello")
+print(x)
+
+txt = "     banana     "
+x = txt.strip()
+print("of all fruits", x, "is my favorite")
+
+txt = "Hello My Name Is PETER"
+x = txt.swapcase()
+print(x)
+
+txt = "Welcome to my world"
+x = txt.title()
+print(x)
+
+txt = "Hello Sam!"
+mytable = txt.maketrans("S", "P")
+print(txt.translate(mytable))
+
+txt = "Hello my friends"
+x = txt.upper()
+print(x)
+
+txt = "50"
+x = txt.zfill(10)
+print(x)
