f3ccace6782f58d74f2d02d657173d077f1cfe72
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-11-09 00:07:38 +0600

    learned rpartition and partition difference

--- 11-String-Methods/string.py
+++ 11-String-Methods/string.py
@@ -5,4 +5,21 @@ cost = 60
 
 message = 'The cost of {2}, {3} and {1} is {0}'
 
-print('\n' + message.format(cost, apple, orange, berry) + '\n')
+print('\n' + message.format(cost, apple, orange, berry))
+
+txt = "I love apples, apple are my favorite fruit"
+
+x = txt.count("apepl")
+
+print(x)
+
+txt = "I could eat bananas all day bananas and Kolas"
+
+x = txt.partition("bananas")
+
+print(x)
+
+x = txt.rpartition("bananas")
+print(x)
+
+print('\n')
