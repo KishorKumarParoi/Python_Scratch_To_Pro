5c7df476259fde1e6a9d395f75068bcc2bdbe40b
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-11-09 01:58:19 +0600

    sorting a list of dictionaries and using two parameter

--- 21-Lists/sorting.py
+++ 21-Lists/sorting.py
@@ -25,3 +25,25 @@ print(numbers)
 
 fruits.sort(key = str.lower)
 print(fruits)
+
+
+def myFunc(e):
+  return e['year']
+
+cars = [
+  {'car': 'Ford', 'year': 2005},
+  {'car': 'Mitsubishi', 'year': 2000},
+  {'car': 'BMW', 'year': 2019},
+  {'car': 'VW', 'year': 2011}
+]
+
+cars.sort(key=myFunc)
+print(cars)
+
+def myFunc(e):
+  return len(e)
+
+cars = ['Ford', 'Mitsubishi', 'BMW', 'VW']
+
+cars.sort(reverse=True, key=myFunc)
+print(cars)
