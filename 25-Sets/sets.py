c6bff95c13dae4c4731c1ae8a42169374a868664
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-11-09 18:01:53 +0600

    learned diff between remove, pop, discard

--- 25-Sets/sets.py
+++ 25-Sets/sets.py
@@ -4,3 +4,22 @@
 my_set = {"apple", "banana", "cherry" , False, 0, 0.0, None, "apple"}
 print(my_set)
 print(len(my_set ))
+
+my_set.add("orange")
+print(my_set)
+
+my_set.update(["orange", "mango", "grapes"])
+print(my_set)
+
+my_set.remove("banana")
+print(my_set)
+
+my_set.discard("banana")
+print(my_set)
+
+my_set.pop()
+print(my_set)
+
+# remove raise error if not found but discard doesn't
+# pop() return deleted item and delete random number
+
