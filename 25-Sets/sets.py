4793b3bacb193da55f853e594c446f9c892eca71
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-11-09 17:53:56 +0600

    sets recognize True and 1, False and 0 same

---
+++ 25-Sets/sets.py
@@ -0,0 +1,6 @@
+# Set is unordered, unchangeable (add, remove), unindexed, and do not allow duplicates
+
+# Create a set
+my_set = {"apple", "banana", "cherry" , False, 0, 0.0, None, "apple"}
+print(my_set)
+print(len(my_set ))
