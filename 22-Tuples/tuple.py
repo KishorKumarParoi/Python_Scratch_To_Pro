9b58a2bc1224047e6e0442fd146d9c2556a71296
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-11-09 17:48:45 +0600

    all tuple methods, accessing, multiplication learned

--- 22-Tuples/tuple.py
+++ 22-Tuples/tuple.py
@@ -7,3 +7,29 @@ print(type(x))
 print(x)
 
 print(len(x))
+
+x = tuple(('apple', 'banana', 'cherry'))
+print(x)
+
+print(fruits[-4:-1])
+
+fruits = list(fruits)
+fruits[1] = 'Pineapple'
+fruits = tuple(fruits)
+print(fruits)
+
+# unpacking tuple
+
+(kkp, *kk, pk) = fruits
+print(kkp)
+print(kk)
+print(pk)
+
+fruits += x;
+print(fruits)
+
+fruits = fruits * 2
+print(fruits)
+
+print(fruits.count('apple'))
+print(fruits.index('apple'))
